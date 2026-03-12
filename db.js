0c937e07588b1fabf330ec57ad381b206f71e41a
qidan liu <qliu0079@student.monash.edu>
2026-03-13 02:49:08 +1100

    search latitude and longitude from table location

--- db.js
+++ db.js
@@ -7,6 +7,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // testing api
 const express = require('express');
+console.log("THIS IS THE NEW SERVER FILE");
 const cors = require('cors');
 const pool = require('./db');
 require('dotenv').config();
@@ -14,6 +15,51 @@ test endpoint
 */
 app.get('/', (req, res) => {
   res.json({ message: "UV API is running" });
+});
+
+// Get UV index by postcode
+app.get('/api/uv/:postcode', async (req, res) => {
+
+  const { postcode } = req.params;
+
+  try {
+
+    // search latitude and longitude from table location
+    const locationResult = await pool.query(
+      `
+      SELECT latitude, longitude
+      FROM location
+      WHERE postcode = $1
+      LIMIT 1
+      `,
+      [postcode]
+    );
+
+    if (locationResult.rows.length === 0) {
+      return res.status(404).json({
+        error: "postcode not found"
+      });
+    }
+
+    const { latitude, longitude } = locationResult.rows[0];
+
+    // return result
+    res.json({
+  postcode,
+  latitude: Number(latitude),
+  longitude: Number(longitude)
+});
+
+  } catch (error) {
+
+    console.error(error);
+
+    res.status(500).json({
+      error: "server error"
+    });
+
+  }
+
 });
 
 const PORT = process.env.PORT || 3000;
